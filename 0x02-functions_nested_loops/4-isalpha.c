11264c19f3a040a3c32a7dff9a5a01052d02216b
Delta707 <dyoldenirs@gmail.com>
2023-03-22 19:52:20 -0700

    Checker 2

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - chacker function
+ *
+ * @c: parameter to be printed
+ * Return: 1 if it is a lower case
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
