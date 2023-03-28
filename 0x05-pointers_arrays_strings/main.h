24e87bdf0cc7394222b1b1a80e298342bc875474
Delta707 <dyoldenirs@gmail.com>
2023-03-28 16:42:49 -0700

    Revease

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 
 void reset_to_98(int *n);
 int _putchar(char c);
@@ -11,5 +12,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
+
 
 #endif
