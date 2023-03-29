f10b609588f2abfefd612e637636f6346d665d07
Delta707 <dyoldenirs@gmail.com>
2023-03-28 17:14:08 -0700

    Even Numbers only

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,22 @@
 void puts2(char *str)
 {
 	int i;
+	int count = 0;
+	int t = 0;
+	char *y = str;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{	_putchar(str[i]);
-		i++;
+	while (*y != '\0')
+	{
+		y++;
+		count++;
+	}
+	t = count - 1;
+	for (i = 0 ; i <= t ; i++)
+	{
+		if (i % 2 == 0)
+	{
+		_putchar(str[i]);
+	}
 	}
 	_putchar('\n');
 }
