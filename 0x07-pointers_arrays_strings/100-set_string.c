5b53eedbd4db50be7b4d08abbf3b3ceda0ee73b8
Delta707 <dyoldenirs@gmail.com>
2023-04-04 19:57:15 -0700

    Change values

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - changes value to char
+ * @s: pointer to other pointer
+ * @to: pointer origin
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
