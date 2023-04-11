e7fd3a2651583582a7955b0a0d8db3a840984df4
Delta707 <dyoldenirs@gmail.com>
2023-04-10 18:10:58 -0700

    Static

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,12 @@
-
 #include "main.h"
+
+/**
+ * main - best hacker in the world
+ * Return: always 0
+ */
 
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day,	my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
