f0f69f25ed2db4792769f5ded738c28e843a7170
Delta707 <dyoldenirs@gmail.com>
2023-04-11 19:46:35 -0700

    alloc Grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * free_grid - free array
+ * @grid: grid stored
+ * @height: grid height
+ * Return: 0
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+	}
