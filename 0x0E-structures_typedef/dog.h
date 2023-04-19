1be535050de3bf22aaa203b5cd3c8c06cb5976f0
Delta707 <dyoldenirs@gmail.com>
2023-04-18 17:02:33 -0700

    Initialize

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,10 +20,10 @@ struct dog
  */
 typedef struct dog dog_t;
 
-void kudo_dog(struct dog *k, char *name, float age, char *owner);
-void print_dog(struct dog *k);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *k);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
